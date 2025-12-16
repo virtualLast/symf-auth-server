07e73c324593ccc5a73597d4dfde410bfcf132ab
virtualLast <mjlast@gmail.com>
2025-12-16 15:37:56 +0000

    only settings email for those providers that have email data available

--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -4,7 +4,9 @@ namespace App\Service;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Foxworth42\OAuth2\Client\Provider\OktaUser;
 use League\OAuth2\Client\Provider\ResourceOwnerInterface;
+use Riskio\OAuth2\Client\Provider\Auth0ResourceOwner;
 use Stevenmaguire\OAuth2\Client\Provider\KeycloakResourceOwner;
 
 readonly class UserService
@@ -31,7 +33,11 @@ readonly class UserService
     {
         $user = new User();
         $user->setTokenSub($remoteUser->getId());
-        if ($remoteUser instanceof KeycloakResourceOwner) {
+        if (
+            $remoteUser instanceof KeycloakResourceOwner
+            || $remoteUser instanceof Auth0ResourceOwner
+            || $remoteUser instanceof OktaUser
+        ) {
             $user->setEmail($remoteUser->getEmail());
         }
         $this->userRepository->save($user);
