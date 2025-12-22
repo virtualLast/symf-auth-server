ce4ab170f7a20e66e91898e91742e400fbf55047
virtualLast <mjlast@gmail.com>
2025-12-22 16:21:34 +0000

    refresh controller stuff set up

---
+++ src/Controller/RefreshController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controller;
+
+use App\OAuth\Exception\OauthException;
+use App\Service\CookieService;
+use App\Service\TokenService;
+use App\Service\UserService;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/refresh')]
+class RefreshController
+{
+    private const REFRESH_TOKEN_REQUEST_ATTRIBUTE = 'refresh_token';
+
+    public function __construct(
+        private readonly TokenService $tokenService,
+        private readonly UserService $userService,
+        private readonly CookieService $cookieService,
+        private readonly LoggerInterface $logger
+    )
+    {
+    }
+
+    /**
+     * @throws \Exception
+     */
+    #[Route('/', name: 'app_refresh')]
+    public function refresh(Request $request): Response
+    {
+        $refreshToken = $request->attributes->get(self::REFRESH_TOKEN_REQUEST_ATTRIBUTE);
+        if($refreshToken === null) {
+            throw new OauthException('Refresh token not found', Response::HTTP_BAD_REQUEST);
+        }
+
+        // call token service to find token
+        $tokenData = $this->tokenService->findByLocalRefreshToken($refreshToken);
+        if($tokenData === null) {
+            $this->logger->error(sprintf('Refresh token (%s) not found', $refreshToken));
+            throw new OauthException('Refresh token not found', Response::HTTP_BAD_REQUEST);
+        }
+        // call to user service to find user, use the user we get from the token
+        $user = $tokenData->getUser();
+        $userObject = $this->userService->findByTokenSub($user->getTokenSub(), $user->getProvider());
+        if($userObject === null) {
+            throw new OauthException('User not found', Response::HTTP_BAD_REQUEST);
+        }
+        // revoke existing refresh and access token
+        $this->tokenService->revokeToken($tokenData->getLocalRefreshToken());
+        // create new refresh and access token
+        $token = $this->tokenService->createSimpleToken();
+        // issue tokens to user
+        $newTokenData = $this->tokenService->issueTokens($token, $user);
+        // create a json response for the access token and set a new cookie for the refresh token
+        $response = new JsonResponse(['access_token' => $newTokenData->getLocalAccessToken()]);
+        $response->headers->setCookie($this->cookieService->createRefresh($newTokenData->getRawLocalRefreshToken(), $newTokenData->getLocalRefreshTokenExpiresAt()));
+        $response->headers->setCookie($this->cookieService->createAccess($newTokenData->getLocalAccessToken(), $newTokenData->getLocalAccessTokenExpiresAt()));
+
+        return $response;
+
+    }
+}
