2eb474a6efce66f99feade2a0ced65947af8fc90
virtualLast <mjlast@gmail.com>
2025-12-22 13:56:25 +0000

    oath exception throwing HttpException so we dont show stack trace

--- src/OAuth/Exception/OauthException.php
+++ src/OAuth/Exception/OauthException.php
@@ -2,7 +2,18 @@
 
 namespace App\OAuth\Exception;
 
-class OauthException extends \Exception
-{
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
+class OauthException extends HttpException
+{
+    public function __construct(
+        string $message = 'Authentication error',
+        int $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR,
+        ?\Throwable $previous = null,
+        array $headers = [],
+        int $code = 0
+    ) {
+        parent::__construct($statusCode, $message, $previous, $headers, $code);
+    }
 }
