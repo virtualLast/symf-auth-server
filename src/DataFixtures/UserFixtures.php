8cd16016bab2c50a8e0d6bafef7c18b797b7ac8e
virtualLast <mjlast@gmail.com>
2026-01-08 16:13:53 +0000

    updated the user fixture

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\User;
+use App\Model\Enum\ProviderEnum;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,11 +11,22 @@ class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+
         for ($i = 0; $i < 10; $i++) {
             $user = new User();
             $user->setEmail(sprintf('user_%s@example.com', $i));
             $user->setTokenSub(sprintf('token_sub_%s', $i));
-            $user->setProvider('keycloak');
+            $user->setProvider(ProviderEnum::KEYCLOAK_LOCAL);
+            $user->setRoles(['ROLE_USER']);
+            $manager->persist($user);
+        }
+
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setEmail(sprintf('login_user_%s@example.com', $i));
+            $user->setTokenSub(sprintf('login_user_token_sub_%s', $i));
+            $user->setPlainTextPassword('password');
+            $user->setProvider(ProviderEnum::LIGHTFOOT);
             $user->setRoles(['ROLE_USER']);
             $manager->persist($user);
         }
@@ -22,28 +34,28 @@ class UserFixtures extends Fixture
         $oidcUser = new User();
         $oidcUser->setEmail('oidc_normal_user@example.local');
         $oidcUser->setTokenSub('oidc_normal_user_1');
-        $oidcUser->setProvider('keycloak');
+        $oidcUser->setProvider(ProviderEnum::KEYCLOAK_LOCAL);
         $oidcUser->setRoles(['ROLE_USER']);
         $manager->persist($oidcUser);
 
         $oidcAdminUser = new User();
         $oidcAdminUser->setEmail('oidc_admin_user@example.local');
         $oidcAdminUser->setTokenSub('oidc_admin_user_1');
-        $oidcAdminUser->setProvider('keycloak');
+        $oidcAdminUser->setProvider(ProviderEnum::KEYCLOAK_LOCAL);
         $oidcAdminUser->setRoles(['ROLE_ADMIN']);
         $manager->persist($oidcAdminUser);
 
         $samlUser = new User();
         $samlUser->setEmail('saml_normal_user@example.local');
         $samlUser->setTokenSub('saml_normal_user_1');
-        $samlUser->setProvider('keycloak');
+        $samlUser->setProvider(ProviderEnum::KEYCLOAK_LOCAL);
         $samlUser->setRoles(['ROLE_USER']);
         $manager->persist($samlUser);
 
         $samlAdminUser = new User();
         $samlAdminUser->setEmail('saml_admin_user@example.local');
         $samlAdminUser->setTokenSub('saml_admin_user_1');
-        $samlAdminUser->setProvider('keycloak');
+        $samlAdminUser->setProvider(ProviderEnum::KEYCLOAK_LOCAL);
         $samlAdminUser->setRoles(['ROLE_ADMIN']);
         $manager->persist($samlAdminUser);
 
